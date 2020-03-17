de0fd0df142af8b716cbf33ac21f81c02ba06f37
anushag1104@gmail.com <anushag1104@gmail.com>
2020-03-17 15:31:08 +0530

    Add project files.

---
+++ MasterLayouts/Controllers/BindController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterLayouts.Controllers
+{
+    public class BindController : Controller
+    {
+        // GET: Bind
+        [ActionName("Example")]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}
